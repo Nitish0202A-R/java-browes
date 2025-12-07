4eb64834722d60160b5580e1dc0d2902167e6fec
Nitish KR Singh <nitishsingh2148@gmail.com>
2025-12-07 11:41:06 +0530

    first commit

--- DOM/project/script.js
+++ DOM/project/script.js
@@ -1,12 +1,11 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1
-let tries = 0
+let tries = 0 // Initialize tries counter
 
 function checkGuess() {
   let userGuess = Number(document.getElementById("guessInput").value) // Convert input to number
   let message = document.getElementById("message")
   let attempts = document.getElementById("attempts")
-
-  tries++;
+  tries++; // Increment tries counter
 
   if (userGuess === randomNumber) {
     message.innerText = " Correct! You guessed the number."

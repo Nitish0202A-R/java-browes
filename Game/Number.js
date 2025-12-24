d2ff120813415339f290fe315d5838bbfa1c6565
Nitish KR Singh <nitishsingh2148@gmail.com>
2025-12-24 22:52:47 +0530

    first commit

---
+++ Game/Number.js
@@ -0,0 +1,18 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+let guess;
+let attempts = 0;
+
+while (guess !== randomNumber) {
+  guess = Number(prompt("Guess a number between 1 to 10"));
+  attempts++;
+
+  if (guess < randomNumber) {
+    alert("Too Low!");
+  }
+  else if (guess > randomNumber) {
+    alert("Too High!");
+  }
+  else {
+    alert("🎉 Correct! You guessed in " + attempts + " attempts.");
+  }
+}

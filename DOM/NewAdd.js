6008cd109f6d0a835f43a93e5b8f6fad615d8a53
Nitish KR Singh <nitishsingh2148@gmail.com>
2025-12-30 02:08:17 +0530

    first commit

--- DOM/NewAdd.js
+++ DOM/NewAdd.js
@@ -1,4 +1,4 @@
-let newPara = document.createElement("p")
-newPara.innerText = "New paragraph added"
+let newPara = document.createElement("p") // Create a new paragraph element
+newPara.innerText = "New paragraph added"// Set the text content of the new paragraph
 
 document.body.appendChild(newPara)

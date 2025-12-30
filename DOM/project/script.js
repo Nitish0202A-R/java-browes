8da82995875ce3b41f6b90a8a681af012f7220d5
Nitish KR Singh <nitishsingh2148@gmail.com>
2025-12-31 00:54:29 +0530

    first commit

---
+++ DOM/project/script.js
@@ -0,0 +1,21 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let tries = 0;
+
+function checkGuess() {
+  let userGuess = Number(document.getElementById("guessInput").value);
+  let message = document.getElementById("message");
+  let attempts = document.getElementById("attempts");
+
+  tries++;
+
+  if (userGuess === randomNumber) {
+    message.innerText = " Correct! You guessed the number.";
+    attempts.innerText = "Attempts: " + tries;
+  } 
+  else if (userGuess > randomNumber) {
+    message.innerText = " Too High! Try again.";
+  } 
+  else {
+    message.innerText = " Too Low! Try again.";
+  }
+}

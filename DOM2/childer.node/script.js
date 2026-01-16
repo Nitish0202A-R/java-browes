2bac50ee358bee0384dcc3efb6bf0b665f344e30
Nitish KR Singh <nitishsingh2148@gmail.com>
2026-01-16 19:55:59 +0530

    first commit

--- DOM2/childer.node/script.js
+++ DOM2/childer.node/script.js
@@ -1,3 +1,6 @@
 console.log(document.body.firstChild)
 console.log(document.body.lastChild)
-console.log(document.body.childNodes)
+console.log(document.body.childNodes)  // gives all child nodes in form of node list
+// array object in indexed collection
+// let arr=Array.from(document.body.childNodes) 
+// console.log(arr)

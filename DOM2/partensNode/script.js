cea3f8c9327247ba101effa9fdff50adf9c8ad07
Nitish KR Singh <nitishsingh2148@gmail.com>
2026-01-17 22:06:47 +0530

    first commit

---
+++ DOM2/partensNode/script.js
@@ -0,0 +1,6 @@
+console.log(document.body.firstElementChild); // firstElementChild gives first element node
+a=document.body.firstElementChild
+console.log(a.parentElement)
+console.log(a.parentNode)
+console.log(document.body.firstElementChild.nextSibling)
+
---
+++ EleNav/script.js
@@ -0,0 +1,6 @@
+const changeBodyRed=()=>{
+    document.body.firstElementChild.style.backgroundColor="red"
+}
+let b= document.body
+console.log("first childd of b is:",b.firstChild)
+console.log("first element child of b is:",b.firstElementChild)

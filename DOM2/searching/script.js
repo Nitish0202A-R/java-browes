f0f1f3492b355361e65404de71ef172cbdefa406
Nitish KR Singh <nitishsingh2148@gmail.com>
2026-01-20 01:17:15 +0530

    first commit

---
+++ DOM2/searching/script.js
@@ -0,0 +1,17 @@
+// Change the color of the titles of the cards using their IDs
+let cardTitle1 = document.getElementById("firstcardtitle")
+cardTitle1.style.color = "gray"
+
+let cardTitle2 = document.getElementById("secondcardtitle")
+cardTitle2.style.color = "blue"
+
+let cardTitle3 = document.getElementById("thirdcardtitle")
+cardTitle3.style.color = "green"
+
+
+document.querySelector(".this").style.color = "red"
+document.querySelector(".this").style.background = "black"
+
+console.log(document.getElementsByTagName("a"))
+
+console.log(document.getElementsByName("search"))

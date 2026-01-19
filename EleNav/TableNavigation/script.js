dd968d8c756d0df5267dc06010678b74f2115033
Nitish KR Singh <nitishsingh2148@gmail.com>
2026-01-19 23:40:18 +0530

    first commit

---
+++ EleNav/TableNavigation/script.js
@@ -0,0 +1,10 @@
+let t= document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead)
+console.log(t.tfood)
+console.log(t.tBodies)
+console.log(tbody.rows)
+console.log(t.rows[1].rowIndex)
+

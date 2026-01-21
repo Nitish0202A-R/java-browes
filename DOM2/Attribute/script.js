74c5e21a8b34430d411748958f601ec42417db17
Nitish KR Singh <nitishsingh2148@gmail.com>
2026-01-21 23:39:17 +0530

    first commit

--- DOM2/Attribute/script.js
+++ DOM2/Attribute/script.js
@@ -1,4 +1,7 @@
-let a= first.getattribute("class")
-console.log(a)
-console.log(first.hasAttribute("class"))
-console.log(first.hasAttribute("style"))
+let first = document.getElementById("first");
+
+let a = first.getAttribute("class");
+console.log(a); // nit
+
+console.log(first.hasAttribute("class")); // true
+console.log(first.hasAttribute("style")); // false
